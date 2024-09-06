57769797e698f155273e73f90f7d6d31cadc16bb
Risky <notriskysh@gmail.com>
2024-09-06 20:54:11 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Simple Compressor</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+	<h1 class="text-5xl font-bold">Hello</h1>
+
+</section>
+
+<style>
+
+</style>
