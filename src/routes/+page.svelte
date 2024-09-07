c5a9f071d718985cdf1e922d57c6088fba0052be
simply zaki <thezakimain@gmail.com>
2024-09-07 15:37:00 +0100

    installed the library and added simple test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,33 @@
 <script>
+  import imageCompression from 'browser-image-compression';
+  let compressedFile;
+
+  async function handleImageUpload(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true
+    };
+
+    try {
+      compressedFile = await imageCompression(file, options);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function downloadImage() {
+    if (!compressedFile) return;
+    const url = URL.createObjectURL(compressedFile);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = compressedFile.name;
+    a.click();
+    URL.revokeObjectURL(url);
+  }
 </script>
 
 <svelte:head>
@@ -8,7 +37,11 @@
 
 <section>
 
-	<h1 class="text-5xl font-bold">Hello</h1>
+	<h1 class="text-5xl font-bold">Simple Compressor</h1>
+
+	<input type="file" accept="image/*" on:change={handleImageUpload} />
+
+	<button class=" bg-stone-500" on:click={downloadImage}>Download Compressed Image</button>
 
 </section>
 
