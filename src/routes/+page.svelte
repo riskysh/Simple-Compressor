27e9d1475b83bbb792ed28d63f654f949af741df
simply zaki <thezakimain@gmail.com>
2024-09-08 20:15:33 +0100

    ui polish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,14 +48,15 @@
 <section class="">
 
   <div class="w-full">
-    <h1 class="text-white text-left ml-28 text-6xl font-semibold mt-20">Simple Compressor</h1>
-    <p class="text-white text-left ml-28 mt-2 text-xl">
+    <h1 class="text-white text-center  text-6xl font-semibold mt-20">Simple Compressor</h1>
+    <p class=" text-gray-200 text-center   mt-7  text-bs">
       Reduce the file size of your files with our Simple Compressor. Compress files quickly and easily.
     </p>
-    <div class="w-10/12 h-72 border border-[#27272a] grid mx-auto rounded-xl mt-10">
-      <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cloud-upload flex mx-auto mt-10"><path d="M12 13v8"/><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"/><path d="m8 17 4-4 4 4"/></svg>
-      <h1 class="text-white text-center relative bottom-2 text-xl">Put your files here to compress</h1>
-      <button class="px-6 py-2 bg-sky-500 grid mx-auto my-auto mb-12 rounded-lg">Choose files</button>
+    <div class="w-5/12 h-72 border-dashed border-2 border border-[#27272a] grid mx-auto rounded-xl mt-10 p-8">
+     <img class="lucide lucide-cloud-upload flex mx-auto" src="/upload.svg" alt="upload">
+      <h1 class="text-white text-center relative bottom-2 text-xl">Drag your files here to compress</h1>
+      <span class=" text-center text-white my-2">Or</span>
+      <button class="px-6 py-2 bg-sky-500 grid mx-auto my-auto  rounded-lg">Choose file</button>
     </div>
   </div>
 
